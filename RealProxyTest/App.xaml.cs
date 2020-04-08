4b4bd9430bd32caa7c68250b1e2d6d996a07f5fb
ak-nkmr <centervil.en@gmail.com>
2020-04-09 00:51:49 +0900

    プロジェクト ファイルを追加します。

---
+++ RealProxyTest/App.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows;
+
+namespace RealProxyTest
+{
+    /// <summary>
+    /// App.xaml の相互作用ロジック
+    /// </summary>
+    public partial class App : Application
+    {
+        [STAThread]
+        public static void Main()
+        {
+            App app = new App();
+            app.InitializeComponent();
+            app.Run();
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            Console.WriteLine("***\r\n Begin program - logging with dynamic proxy\r\n");
+            IRepository<Customer> customerRepository =
+              RepositoryFactory.Create<Customer>();
+            Customer customer = new Customer
+            {
+                Id = 1,
+                Name = "Customer 1",
+                Address = "Address 1"
+            };
+            customerRepository.Add(customer);
+            customerRepository.Update(customer);
+            customerRepository.Delete(customer);
+            Console.WriteLine("\r\nEnd program - logging with dynamic proxy\r\n***");
+            Console.ReadLine();
+        }
+    }
+}
